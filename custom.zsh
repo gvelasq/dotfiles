ba228a8b21a48719130859ae561d09283b21b014
Gustavo Velásquez <gvelasq2github@gmail.com>
2020-09-24 17:15:41 -0400

    Add custom.zsh

---
+++ custom.zsh
@@ -0,0 +1,21 @@
+# aliases
+alias bc="brew cleanup"
+alias bci="brew cask info"
+alias bcin="brew cask install"
+alias bcl="brew cask list"
+alias bco="brew cask outdated"
+alias bcr="brew cask reinstall"
+alias bcu="brew cask upgrade"
+alias bi="brew info"
+alias bl="brew list"
+alias bo="brew outdated"
+alias bu="brew update"
+alias bup="brew upgrade"
+alias bubco="brew update && brew cask outdated"
+alias bubobco="brew update && brew outdated && brew cask outdated"
+alias glon="git log --oneline -n 5"
+alias gs="git status"
+alias lst="ls -alht"
+
+# shortcuts
+omz=~/.oh-my-zsh/custom/custom.zsh
